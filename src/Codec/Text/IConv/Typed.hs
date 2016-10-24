b2926cd8d8ce1c2d7403da8970d697b1be35f303
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2016-10-24 20:55:16 +0200

    2 APIs

--- src/Codec/Text/IConv/Typed.hs
+++ src/Codec/Text/IConv/Typed.hs
@@ -1,17 +1,17 @@
-{-# LANGUAGE TypeInType #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 module Codec.Text.IConv.Typed
   ( E(..)
+#if __GLASGOW_HASKELL__ > 800
   , Enc(..)
+#endif
   , ValidEncoding
   , convert
-  , convert'
   , convertFuzzy
   , convertStrictly
   , convertLazily
@@ -24,76 +24,9 @@ module Codec.Text.IConv.Typed
 
 import qualified Codec.Text.IConv as I
 import           Codec.Text.IConv.Typed.TH
-import           Data.ByteString.Lazy
-import           Data.String
-import           GHC.TypeLits
 
-$(generateEncodings)
-
-newtype Enc k1 k2 = Enc ByteString
-
-instance IsString (Enc k1 k2) where
-  fromString x = Enc (fromString x)
-
---------------------------------------------------------------------------------
-convert' :: ( KnownSymbol k1
-            , KnownSymbol k2
-            , ValidEncoding k1 ~ 'True
-            , ValidEncoding k2 ~ 'True
-            )
-         => Enc (k1 :: Symbol) (k2 :: Symbol) -- ^ Input text
-         -> ByteString -- ^ Output text
-convert' (Enc input :: Enc k1 k2) = I.convert (reifyEncoding (E @k1))
-                                    (reifyEncoding (E @k2)) input
-
---------------------------------------------------------------------------------
-convert :: ( KnownSymbol k1
-            , KnownSymbol k2
-            , ValidEncoding k1 ~ 'True
-            , ValidEncoding k2 ~ 'True
-            )
-         => E k1       -- ^ Name of input string encoding
-         -> E k2       -- ^ Name of output string encoding
-         -> ByteString -- ^ Input text
-         -> ByteString -- ^ Output text
-convert e1 e2 input = I.convert (reifyEncoding e1) (reifyEncoding e2) input
-
---------------------------------------------------------------------------------
-convertFuzzy :: ( KnownSymbol k1
-                , KnownSymbol k2
-                , ValidEncoding k1 ~ 'True
-                , ValidEncoding k2 ~ 'True
-                )
-             => I.Fuzzy -- ^ Whether to try and transliterate or
-                        -- discard characters with no direct conversion
-             -> E k1    -- ^ Name of input string encoding
-             -> E k2    -- ^ Name of output string encoding
-             -> ByteString    -- ^ Input text
-             -> ByteString    -- ^ Output text
-convertFuzzy fuzzy fromEncoding toEncoding input =
-  I.convertFuzzy fuzzy (reifyEncoding fromEncoding) (reifyEncoding toEncoding) input
-
---------------------------------------------------------------------------------
-convertStrictly :: ( KnownSymbol k1
-                   , KnownSymbol k2
-                   , ValidEncoding k1 ~ 'True
-                   , ValidEncoding k2 ~ 'True
-                   )
-                => E k1                                -- ^ Name of input string encoding
-                -> E k2                                -- ^ Name of output string encoding
-                -> ByteString                          -- ^ Input text
-                -> Either ByteString I.ConversionError -- ^ Output text or conversion error
-convertStrictly fromEncoding toEncoding input =
-  I.convertStrictly (reifyEncoding fromEncoding) (reifyEncoding toEncoding) input
-
---------------------------------------------------------------------------------
-convertLazily :: ( KnownSymbol k1
-                 , KnownSymbol k2
-                 , ValidEncoding k1 ~ 'True
-                 , ValidEncoding k2 ~ 'True
-                 )
-              => E k1  -- ^ Name of input string encoding
-              -> E k2  -- ^ Name of output string encoding
-              -> ByteString  -- ^ Input text
-              -> [I.Span]        -- ^ Output text spans
-convertLazily fromEncoding toEncoding input = I.convertLazily (reifyEncoding fromEncoding) (reifyEncoding toEncoding) input
+#if __GLASGOW_HASKELL__ > 800
+import           Codec.Text.IConv.Typed.TypeInTypeAPI
+#else
+import           Codec.Text.IConv.Typed.VerboseAPI
+#endif
